1758a2460567ef0466b07363d141da042ac73c61
Harry <harrisnkm@gmail.com>
2021-01-11 21:22:06 +1000

    Adds CRA frontend

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+import {Button} from 'carbon-components-react'
+
+function App() {
+  return (
+    <div className="App">
+     <p>This is the App</p>
+        <Button>Create React Component</Button>
+    </div>
+  );
+}
+
+export default App;
