572686799d1be53e2644440e65efb38ce04a1e93
Harry <harrisnkm@gmail.com>
2021-01-14 13:46:40 +1000

    BugFix for yfinance forked repo

--- stocks_handler/views.py
+++ stocks_handler/views.py
@@ -32,6 +32,12 @@ def _orient_records_to_list(df):
     return_json = json.loads(df_json_str)
     return _success_response(return_json)
 
+def _df_to_list(df):
+    df_json_str = df.to_json()
+    return_json = json.loads(df_json_str)
+    return _success_response(return_json)
+
+
 def _is_df_empty(df):
     '''Checks if dataframe is empty
     may not need this'''
@@ -79,11 +85,12 @@ def stock_splits(request, ticker):
 
 
 def stock_financials(request, ticker):
-    '''returns empty DF'''
+    '''Convert Unix Timestamp'''
     stock = Ticker(ticker)
     quarterly = _is_quarterly(request.path)
-    return_json = stock.financials(quarterly)
-    return HttpResponse(return_json)
+    df = stock.financials(quarterly)
+    return_json = _df_to_list(df)
+    return JsonResponse(return_json)
 
 
 def stock_major_holders(request, ticker):
@@ -104,7 +111,8 @@ def stock_balance_sheet(request, ticker):
     '''returns empty DF'''
     stock = Ticker(ticker)
     quarterly = _is_quarterly(request.path)
-    return_json = stock.balance_sheet(quarterly)
+    df = stock.balance_sheet(quarterly)
+    return_json = _df_to_list(df)
     return JsonResponse(return_json)
 
 
@@ -112,7 +120,8 @@ def stock_cashflow(request, ticker):
     '''returns empty DF'''
     stock = Ticker(ticker)
     quarterly = _is_quarterly(request.path)
-    return_json = stock.cashflow(quarterly)
+    df = stock.cashflow(quarterly)
+    return_json = _df_to_list(df)
     return JsonResponse(return_json)
 
 
@@ -120,14 +129,15 @@ def stock_earnings(request, ticker):
     '''returns empty DF'''
     stock = Ticker(ticker)
     quarterly = _is_quarterly(request.path)
-    return_json = stock.earnings(quarterly)
+    df = stock.earnings(quarterly)
+    return_json = _df_to_list(df)
     return JsonResponse(return_json)
 
 
 def stock_sustainability(request, ticker):
     stock = Ticker(ticker)
     df = stock.sustainability()
-    return_json = _orient_records_to_list(df)
+    return_json = _df_to_list(df)
     return JsonResponse(return_json)
 
 
@@ -142,24 +152,25 @@ def stock_calendar_events(request, ticker):
     '''Returns DataFrame'''
     stock = Ticker(ticker)
     df = stock.calendar_events()
-    return_json = _orient_records_to_list(df)
-    return HttpResponse(return_json)
+    return_json = _df_to_list(df)
+    return JsonResponse(return_json)
 
 
 def stock_isin(request, ticker):
-    '''returns ISIN = International Securities Identification Number
-    String
+    '''returns ISIN = International Securities Identification Number (String)
     '''
     stock = Ticker(ticker)
-    return_json = stock.isin()
+    isin_str = stock.isin()
+    return_json = _success_response(isin_str)
     return HttpResponse(return_json)
 
 
 def stock_options(request, ticker):
     '''Returns a tuple of expiration dates'''
     stock = Ticker(ticker)
-    return_json = stock.options()
-    return HttpResponse(return_json)
+    options_tuple = stock.options()
+    return_json = _success_response(options_tuple)
+    return JsonResponse(return_json)
 
 
 def stock_option_chain(request, ticker, expiration, option):
